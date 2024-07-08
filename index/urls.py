abc3b4f6737229d2811eec7c27782993f4ef5bcf
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-07-08 14:48:17 +0530

    second monday

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('',views.index,name='index_url'),
-    path('login/',views.login,name='login_url'),
-    path('addNew/',views.addNew,name='addNew_url')
+urlpatterns = [
+    path('', views.index, name='index_url'),
+    path('addData/', views.addData, name='addData_url'),
+    path('view/', views.viewDetails, name='view_url'),
+    path('addNew/', views.addNew, name='addNew_url'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+from .models import empdata
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+def addData(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        department = request.POST.get('department')
+        salary = request.POST.get('salary')
+        print(name, age, salary, email, department, phone)
+
+        data = empdata(name=name, age=age, email=email, phone=phone, department=department, salary=salary)
+        data.save()
+        return render(request, 'addNew.html')
 
 def addNew(request):
-    return render(request,'addNew.html')
+    return render(request, 'addNew.html')
 
-def login(request):
-    return render(request,'login.html')
+def viewDetails(request):
+    mydata = empdata.objects.filter(name='bhavesh ').values()
+    return render(request, 'view.html', context={'data': mydata})
+
