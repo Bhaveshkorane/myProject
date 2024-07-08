a2aff87c89b631a293c1fe3f60ba7ab67d38e826
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-07-08 09:24:40 +0530

    monday

--- index/urls.py
+++ index/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index),
-    path('login/',views.login),
-    path('addNew/',views.addNew)
+    path('',views.index,name='index_url'),
+    path('login/',views.login,name='login_url'),
+    path('addNew/',views.addNew,name='addNew_url')
 ]
